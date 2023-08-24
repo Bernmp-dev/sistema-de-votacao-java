a21a419e9ae388c7a395c6580432a5bda4b2c5a9
eon <bernardomp.dev@gmail.com>
2023-08-24 15:02:17 -0300

    Feat: [GerenciamentoVotacao] Error:   cadastrarPessoaCandidata.

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -1,6 +1,37 @@
 package com.betrybe.sistemadevotacao;
 
-/** GerenciamentoVotacao Concrte Class. */
-public class GerenciamentoVotacao {
+import java.util.ArrayList;
 
+/** GerenciamentoVotacao Concrete Class. */
+public class GerenciamentoVotacao implements GerenciamentoVotacaoInterface {
+  private ArrayList<PessoaCandidata> pessoasCandidatas = new ArrayList<>();
+  private ArrayList<PessoaEleitora> pessoasEleitoras = new ArrayList<>();
+  private ArrayList<PessoaEleitora> cpfsComputados = new ArrayList<>();
+
+  public void cadastrarPessoaCandidata(String nome, int numero) {
+    for (PessoaCandidata pessoa : pessoasCandidatas) {
+      if (pessoa.getNumero() == numero) {
+        System.out.println("Número da pessoa candidata já utilizado!");
+        return;
+      }
+    }
+//    System.out.println();
+    PessoaCandidata novaPessoaCandidata = new PessoaCandidata(nome, numero);
+    pessoasCandidatas.add(novaPessoaCandidata);
+  }
+
+  @Override
+  public void cadastrarPessoaEleitora(String nome, String cpf) {
+
+  }
+
+  @Override
+  public void votar(String cpfPessoaEleitora, int numeroPessoaCandidata) {
+
+  }
+
+  @Override
+  public void mostrarResultado() {
+
+  }
 }
