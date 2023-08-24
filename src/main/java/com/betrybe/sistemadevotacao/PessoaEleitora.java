f7cfbf1761e78f987c9d4178588cafdd60460dcd
eon <bernardomp.dev@gmail.com>
2023-08-24 11:34:54 -0300

    Feat: PessoaEleitora ConcreteClass.

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,20 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaEleitora{
+/** PessoaEleitora. */
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    super();
+    super.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
