7ad6105e3c15c6455f242353f22159dc4f8f582c
eon <bernardomp.dev@gmail.com>
2023-08-24 11:14:25 -0300

    Feat: PessoaCandidata Class.

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int votos;
+  private int numero;
 
+  public PessoaCandidata(String nome, int numero) {
+    super();
+    this.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public void receberVoto() {
+    setVotos(1);
+  }
 }
